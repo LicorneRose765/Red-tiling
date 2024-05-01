f04ed458e9f90ee7412e6c19b512d2bc693a2cd4
LicorneRose765 <210376@umons.ac.be>
2024-05-01 20:16:33 +0200

    Working script for solution

--- src/script.py
+++ src/script.py
@@ -50,6 +50,7 @@ def read_db_file(db_file):
 
     # Returns:
     - board: a numpy array representing the board
+    - pieces: a list of tuples (piece_type, number_of_pieces)
     """
     with open(db_file, "r") as f:
         lines = f.readlines()
@@ -95,39 +96,17 @@ def print_board(np_array):
     """
     print the board in the terminal
     """
-    for i in range(np_array.shape[0]-1, -1, -1):
+    for i in range(np_array.shape[0]):
         for j in range(np_array.shape[1]):
             if np_array[i][j] == 0:
                 print("W", end=" ")
-            elif np_array[i][j] == 1:
-                print("R", end=" ")
             elif np_array[i][j] == 2:
+                print("R", end=" ")
+            elif np_array[i][j] == 1:
                 print("B", end=" ")
+            else:
+                print(np_array[i][j] - 2, end=" ")
         print()
-
-
-def display_board(np_array, name="Board"):
-    """
-    Display the board in a window
-    """
-
-    # Define custom colormap
-    colors = ['white', 'blue', 'red']  # Add more colors as needed
-
-    # Create a custom colormap
-    cmap_custom = ListedColormap(colors)
-
-    fig, ax = plt.subplots()
-    extent = [0, np_array.shape[1], 0, np_array.shape[0]]
-    ax.imshow(np_array, cmap=cmap_custom, interpolation="nearest", extent=extent)
-
-    # Force integer ticks
-    ax.set_xticks(np.arange(0, np_array.shape[1], 1))
-    ax.set_yticks(np.arange(0, np_array.shape[0], 1))
-
-    plt.grid(True)
-    plt.title(name.split(".")[0].split("/")[-1])
-    plt.show()
 
 
 def get_board_for_piece(piece_type):
@@ -197,6 +176,106 @@ def display_pieces(pieces):
     plt.show()
 
 
+def read_sol(solution):
+    """
+    Read a solution string and return a tuple (piece_type, x, y)
+    
+    - Input : example "one_sol(T, X, Y)" or "two_sol(T, R, X, Y)".
+
+    - Returns : (piece_type, tiles) where tiles is a list of tuples (x, y)
+    """
+    piece_type = None
+    tiles = []
+    split = solution.split(",")
+    
+    if len(split) == 3:
+        # One
+        piece_type = 1
+        x = int(split[1].strip())
+        y = int(split[2].strip()[:-1])
+        tiles.append((x, y))
+    elif len(split) >= 6 or len(split) <= 10:
+        # Two
+        piece_type = int(split[0].split("(")[1].strip())
+        for i in range(2, len(split), 2):
+            if i == len(split) - 2:
+                x = int(split[i].strip())
+                y = int(split[i+1].strip()[:-1])
+                tiles.append((x, y))
+            else:
+                x = int(split[i].strip())
+                y = int(split[i+1].strip())
+                tiles.append((x, y))
+    
+    
+    return (piece_type, tiles)
+
+
+def add_solutions_to_board(board_old, solution_list):
+    """
+    Read a solution string and update the board accordingly
+    """
+    # Copy the board
+    board = board_old.copy()
+    m = board.shape[0] # Number of rows
+
+    for solution in solution_list:
+        piece_type, tiles = read_sol(solution)
+        for tile in tiles:
+            x, y = tile
+            board[m - y - 1][x] = piece_type + 2  # Add 2 to differentiate the pieces from the board
+
+    return board       
+
+
+def display_board(board, name="Board"):
+    # Create a custom colormap
+    color_map = {
+        0: np.array([255,255,255]), # white
+        1: np.array([0,0,255]), # blue
+        2: np.array([255,0,0]), # red
+        # random color for each piece
+        3: np.array([255,255,0]), # yellow type 1
+        4: np.array([0,255,0]), # green type 2
+        5: np.array([255,0,255]), # pink type 3 
+        6: np.array([0,255,255]), # cyan type 4
+        7: np.array([255,128,0]), # orange type 5
+        8: np.array([128,0,255]), # purple type 6
+        9: np.array([0,128,255]), # light blue type 7
+        10: np.array([128,255,0]), # light green type 8
+        11: np.array([255,0,128]), # dark pink type 9 
+        12: np.array([0,255,128]), # dark cyan type 10
+        13: np.array([128,255,255]), # light cyan type 11
+    }
+
+    # make a 3d numpy array that has a color channel dimension
+    data_3d = np.ndarray(shape=(board.shape[0], board.shape[1], 3), dtype=int)
+    for i in range(0, board.shape[0]):
+        for j in range(0, board.shape[1]):
+            data_3d[i][j] = color_map[board[i][j]]
+
+    # display the plot
+    fig, ax = plt.subplots(1, 1)
+    extent = [0, board.shape[1], 0, board.shape[0]]
+    ax.imshow(data_3d, interpolation="nearest", extent=extent)
+
+    n = board.shape[1]
+    m = board.shape[0]
+    for i in range(0, board.shape[0]):
+        for j in range(0, board.shape[1]):
+            c = board[i][j]
+            if c > 2:
+                ax.text(j, m-i-1, str(c-2), color='black', fontsize=12, fontweight='bold')
+
+    # Force integer ticks
+    ax.set_xticks(np.arange(0, board.shape[1], 1))
+    ax.set_yticks(np.arange(0, board.shape[0], 1))
+
+    plt.grid(True)
+    plt.title(name.split(".")[0].split("/")[-1])
+    plt.show()
+
+
 if __name__ == "__main__":
     import sys
 
@@ -213,3 +292,11 @@ if __name__ == "__main__":
     print_board(board)
     display_board(board, db_file)
     display_pieces(pieces)
+
+    print(board)
+
+    board[3][1] = 3
+    board[3][2] = 3
+    board[3][3] = 3
+    print(board)
+    display_board(board, db_file)
